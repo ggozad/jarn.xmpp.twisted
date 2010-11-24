5db87165066da78a1d94f1608c9b62d072d413ab
Yiorgis Gozadinos <ggozad@jarn.com>
2010-11-24 09:44:09 +0100

    Proper twisted integration

--- plone/messaging/twisted/testing.py
+++ plone/messaging/twisted/testing.py
@@ -4,6 +4,10 @@ from plone.app.testing import PLONE_FIXTURE
 from plone.app.testing import IntegrationTesting, FunctionalTesting
 
 
+from zope.component import getUtility, getSiteManager
+from plone.messaging.twisted.interfaces import IZopeReactor
+from plone.messaging.twisted.reactor import ZopeReactor
+
 class ReactorFixture(PloneSandboxLayer):
 
     defaultBases = (PLONE_FIXTURE,)
@@ -12,7 +16,28 @@ class ReactorFixture(PloneSandboxLayer):
         # Load ZCML
         import plone.messaging.twisted
         xmlconfig.file('configure.zcml', plone.messaging.twisted, context=configurationContext)
+        gsm = getSiteManager()
+        zr = getUtility(IZopeReactor)
+        #import pdb; pdb.set_trace( )
+        #zr.stop()
+        #gsm.unregisterUtility(zr)
 
+    def testSetUp(self):
+        #pass
+        #gsm = getSiteManager()
+        #gsm.registerUtility(ZopeReactor())
+        zr = getUtility(IZopeReactor)
+        from plone.messaging.twisted.interfaces import ReactorStarted
+        from zope.event import notify
+        notify(ReactorStarted(zr.reactor))
+        # #zr.start()
+        #import pdb; pdb.set_trace( )
+    #def testTearDown(self):
+        #pass
+        #zr = getUtility(IZopeReactor)
+        #zr.stop()
+        #gsm = getSiteManager()
+        #gsm.unregisterUtility(zr)
 
 REACTOR_FIXTURE = ReactorFixture()
 
