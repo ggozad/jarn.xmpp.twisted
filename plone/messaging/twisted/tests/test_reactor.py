22256a6f4cdcab5b90f127ea2e10d4a870934d2f
Yiorgis Gozadinos <ggozad@jarn.com>
2010-11-26 15:50:34 +0100

    Fixed reactor tests

--- plone/messaging/twisted/tests/test_reactor.py
+++ plone/messaging/twisted/tests/test_reactor.py
@@ -12,20 +12,6 @@ class ReactorTests(unittest.TestCase):
         zr = getUtility(IZopeReactor)
         self.failUnless(zr.reactor.running)
 
-    def test_reactor_accepts_deferreds(self):
-        def times_two(x):
-            d = defer.Deferred()
-            zr = getUtility(IZopeReactor).reactor
-            zr.callLater(0, d.callback, x * 2)
-            return d
-
-        def cb(result):
-            self.assertEqual(result, 4)
-
-        d = times_two(2)
-        d.addCallback(cb)
-        return d
-
     def test_stop_start(self):
         zr = getUtility(IZopeReactor)
         zr.stop()
