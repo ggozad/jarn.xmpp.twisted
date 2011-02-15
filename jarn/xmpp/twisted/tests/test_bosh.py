4b468c0c0174e126c657257ea6b77ca81e9daf83
Yiorgis Gozadinos <ggozad@jarn.com>
2011-02-15 16:18:33 +0100

    Added test for bosh

---
+++ jarn/xmpp/twisted/tests/test_bosh.py
@@ -0,0 +1,22 @@
+import random
+import unittest2 as unittest
+
+from twisted.words.protocols.jabber.jid import JID
+
+from jarn.xmpp.twisted.httpb import BOSHClient
+from jarn.xmpp.twisted.testing import REACTOR_INTEGRATION_TESTING
+
+
+class ClientNetworkTest(unittest.TestCase):
+
+    layer = REACTOR_INTEGRATION_TESTING
+
+    def test_bosh_authentication(self):
+        jid = JID('admin@localhost')
+        jid.resource = str(random.randint(0, 1000))
+        client = BOSHClient(jid, 'admin', 'http://localhost:5280/http-bind/')
+        self.assertTrue(client.startSession())
+        rid = client.rid
+        sid = client.sid
+        self.assertTrue(rid is not None)
+        self.assertTrue(sid is not None)
