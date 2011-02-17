fd4972c2c993b7296edf958ff441ae8774b08953
Yiorgis Gozadinos <ggozad@jarn.com>
2011-02-17 14:01:53 +0100

    Added component base class.

---
+++ jarn/xmpp/twisted/component.py
@@ -0,0 +1,71 @@
+from twisted.words.protocols.jabber import component
+from twisted.words.xish import domish
+from zope.component import getUtility
+from wokkel.subprotocols import StreamManager
+
+from jarn.xmpp.twisted.interfaces import IZopeReactor
+
+
+class XMPPComponent(StreamManager):
+
+    def __init__(self, host, port, domain, password):
+        self.host = host
+        self.port = port
+
+        self._state = None
+        factory = component.componentFactory(domain, password)
+
+        StreamManager.__init__(self, factory)
+
+        self._state = u'connecting'
+        zr = getUtility(IZopeReactor)
+        zr.reactor.callFromThread(self.connect)
+
+    def _authd(self, xs):
+        #Set JID
+        self.jid = self.xmlstream.thisEntity
+
+        #Patch send to always include from.
+        old_send = xs.send
+
+        def send(obj):
+            if domish.IElement.providedBy(obj) and \
+                    not obj.getAttribute('from'):
+                obj['from'] = self.jid.full()
+            old_send(obj)
+
+        xs.send = send
+
+        StreamManager._authd(self, xs)
+        self._state = u'authenticated'
+
+    def connect(self):
+        zr = getUtility(IZopeReactor)
+        self._connector = zr.reactor.connectTCP(self.host,
+                                                self.port,
+                                                self.factory)
+
+    def disconnect(self):
+        self.xmlstream.sendFooter()
+        self._connector.disconnect()
+
+    def _connected(self, xs):
+        self._state = u'connected'
+        super(XMPPComponent, self)._connected(xs)
+
+    def _disconnected(self, _):
+        self._state = u'disconnected'
+        super(XMPPComponent, self)._disconnected(_)
+
+    @property
+    def state(self):
+        return self._state
+
+    def initializationFailed(self, reason):
+        """
+        Called when stream initialization has failed.
+
+        Disconnect the current stream and raise the exception.
+        """
+        self.disconnect()
+        reason.raiseException()
---
+++ jarn/xmpp/twisted/tests/test_component.py
@@ -0,0 +1,21 @@
+import unittest2 as unittest
+
+from twisted.words.protocols.jabber.jid import JID
+
+from jarn.xmpp.twisted.component import XMPPComponent
+from jarn.xmpp.twisted.testing import REACTOR_INTEGRATION_TESTING
+from jarn.xmpp.twisted.testing import wait_for_client_state
+
+
+class ComponentNetworkTest(unittest.TestCase):
+
+    layer = REACTOR_INTEGRATION_TESTING
+
+    def test_component_connection(self):
+
+        component = XMPPComponent('localhost',
+                                  5347,
+                                  'example.localhost',
+                                  'secret')
+        self.assertTrue(wait_for_client_state(component, 'authenticated'))
+        self.assertEqual(component.jid, JID('example.localhost'))
